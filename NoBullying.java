a711bfb747ca8e8102f467a01586f63adfd9145b
THIAGO TRINDADE DE FARIAS <393008927@alunos.etec.itaqua>
2019-03-14 20:20:14 -0300

    correção das aspas

--- NoBullying.java
+++ NoBullying.java
@@ -1,25 +1,25 @@
 public class NoBullying {
     public static void main(String[] args){
-        System.out.println('Programa NoBullyng');
+        System.out.println("Programa NoBullyng");
 
-        System.out.println('Passo 1: Sempre manter o respeito com o proximo');
+        System.out.println("Passo 1: Sempre manter o respeito com o proximo");
 
-        System.out.println('Passo 2: Praticar o ato de elogiar as pequenas atitudes');
+        System.out.println("Passo 2: Praticar o ato de elogiar as pequenas atitudes");
 
-        System.out.println('Passo 3: Conversar sempre com os filhos');
+        System.out.println("Passo 3: Conversar sempre com os filhos");
 
-        System.out.println('Passo 4: Abrir discussões relacionadas ao tema em escolas');
+        System.out.println("Passo 4: Abrir discussões relacionadas ao tema em escolas");
 
-        System.out.println('Passo 5: Incentivar a pratica de atividades extra curriculares');
+        System.out.println("Passo 5: Incentivar a pratica de atividades extra curriculares");
 
-        System.out.println('Passo 6: Manter conversas sobre os acontecimentos da escola com os pais');
+        System.out.println("Passo 6: Manter conversas sobre os acontecimentos da escola com os pais");
 
-        System.out.println('Passo 7: Saber lidar com as divergencias culturais');
+        System.out.println("Passo 7: Saber lidar com as divergencias culturais");
 
-        System.out.println('Passo 8: Observar o comportamento das pessoas ao seu redor');
+        System.out.println("Passo 8: Observar o comportamento das pessoas ao seu redor");
 
-        System.out.println('Passo 9: Fornecer auxilio');
+        System.out.println("Passo 9: Fornecer auxilio");
 
-        System.out.println('Passo 10: Ter empatia');
+        System.out.println("Passo 10: Ter empatia");
     }
 }
